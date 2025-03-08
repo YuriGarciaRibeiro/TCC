53319effbedc893e14547056af5b61cbc3b18609
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2025-03-08 14:47:16 -0300

    Adicionar arquivo de detalhes para vídeo de ator com parâmetros de entrada e saída

--- src/Suport/analise.py
+++ src/Suport/analise.py
@@ -1,9 +1,10 @@
 import os
+import shutil
 import subprocess
 from time import sleep
 
-input_dir = os.path.expanduser('/Users/yurigarciaribeiro/Documents/TCC/DockerC/input/')
-output_dir = os.path.expanduser('/Users/yurigarciaribeiro/Documents/TCC/DockerC/output/')
+input_dir = os.path.expanduser('C:/Users/yurig/Documents/TCC/DockerC/input/')
+output_dir = os.path.expanduser('C:/Users/yurig/Documents/TCC/DockerC/output/')
  
 for raiz, _, arquivos in os.walk(input_dir, topdown=False):
     for video in arquivos[::-1]:
@@ -19,5 +20,12 @@ for raiz, _, arquivos in os.walk(input_dir, topdown=False):
             
             subprocess.run(command, cwd=raiz, check=True)
             
+            # copa o arquivo para a pasta de saída
+            shutil.copy2(os.path.join(raiz, video), os.path.join(output_dir, raiz.split('/')[-1], video))
+            print(f"Arquivo {video} copiado para a pasta de saída")
+        elif video.endswith('.wav'):
+            #copia o arquivo para a pasta de saída
+            shutil.copy2(os.path.join(raiz, video), os.path.join(output_dir, raiz.split('/')[-1], video))
+            print(f"Arquivo {video} copiado para a pasta de saída")
             
 print("Processamento concluído.")
--- src/emotion_analysis/analyze_video_emotions.py
+++ src/emotion_analysis/analyze_video_emotions.py
@@ -1,21 +1,26 @@
 import os
-from matplotlib.offsetbox import AnnotationBbox, OffsetImage
-import pandas as pd
+
 import matplotlib.pyplot as plt
+import pandas as pd
 from emotion_aus import emotions_au
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 from PIL import Image
+
 
 # Função para listar todos os arquivos recursivamente
 def list_all_files_in_directory(directory_path):
     all_files = []
-    
+
     # Percorre o diretório e subdiretórios
     for root, dirs, files in os.walk(directory_path):
         # Para cada arquivo, adiciona o caminho completo à lista
         for file in files:
-            all_files.append(os.path.join(root, file))  # Adiciona o caminho completo do arquivo
-    
+            all_files.append(
+                os.path.join(root, file)
+            )  # Adiciona o caminho completo do arquivo
+
     return all_files
+
 
 # Função para analisar as emoções em um vídeo
 def analyze_video_emotions(video_path, threshold=0.6):
@@ -23,16 +28,18 @@ def analyze_video_emotions(video_path, threshold=0.6):
     df = pd.read_csv(video_path)
 
     # Filtrar as colunas AU (intensidade)
-    filtered_df = df[[col for col in df.columns if col.startswith(" AU") and col.endswith("r")]]
+    filtered_df = df[
+        [col for col in df.columns if col.startswith(" AU") and col.endswith("r")]
+    ]
 
     # Dicionário para armazenar as emoções atribuídas aos frames
     frame_emotions = {}
 
     # Criar um dicionário de contagem das emoções para visualização
     emotion_counts = {emotion: 0 for emotion in emotions_au}
-    
+
     dominant_frame = 0
-    
+
     # Iterar sobre as linhas e somar os AUs para cada emoção
     for idx, row in filtered_df.iterrows():
         # Dicionário para armazenar a soma das AUs por emoção
@@ -42,7 +49,9 @@ def analyze_video_emotions(video_path, threshold=0.6):
         for emotion, aus in emotions_au.items():
             for au in aus:
                 col_name = f" AU{au:02}_r"
-                if col_name in row and row[col_name] > threshold:  # Verifica se o AU está ativo
+                if (
+                    col_name in row and row[col_name] > threshold
+                ):  # Verifica se o AU está ativo
                     emotion_sums[emotion] += row[col_name]
 
         # A emoção com a maior soma será a atribuída ao frame
@@ -53,15 +62,15 @@ def analyze_video_emotions(video_path, threshold=0.6):
 
         # Atualizar a contagem de ativações das emoções
         emotion_counts[dominant_emotion] += 1
-        
+
         # Atualizar o frame que teve a maior expressão emocional
         if emotion_sums[dominant_emotion] > emotion_sums.get(dominant_frame, 0):
             dominant_frame = idx
-    
+
     # /Users/yurigarciaribeiro/Documents/TCC/DockerC/output/Actor01/full-AV-speech-angry-strong-Kids-are-talking-by-the-door-1st-repetition-actor01.mp4/full-AV-speech-angry-strong-Kids-are-talking-by-the-door-1st-repetition-actor01_aligned
-    
+
     # Caminho da imagem do frame
-    dominant_frame_path = "/Users/yurigarciaribeiro/Documents/TCC/DockerC/output/Actor01/full-AV-speech-angry-strong-Kids-are-talking-by-the-door-1st-repetition-actor01.mp4/full-AV-speech-angry-strong-Kids-are-talking-by-the-door-1st-repetition-actor01_aligned/frame_det_00_000001.bmp"
+    dominant_frame_path = f"{video_path.replace('.csv', '')}_aligned/frame_det_00_{str(dominant_frame).zfill(6)}.bmp"
 
     # Encontrar a emoção predominante no vídeo (a que mais apareceu)
     dominant_emotion_video = max(emotion_counts, key=emotion_counts.get)
@@ -75,21 +84,30 @@ def analyze_video_emotions(video_path, threshold=0.6):
 
     # Título geral da figura
     video_name = os.path.basename(video_path)
-    plt.suptitle(f'Contagem das Emoções ao Longo do Vídeo: {video_name}', fontsize=16)
+    plt.suptitle(f"Contagem das Emoções ao Longo do Vídeo: {video_name}", fontsize=16)
 
     # Gráfico de barras no primeiro subplot
     ax1.bar(emotion_counts.keys(), emotion_counts.values())
-    ax1.set_xlabel('Emoções')
-    ax1.set_ylabel('Número de ativações')
-    ax1.tick_params(axis='x', rotation=45)
+    ax1.set_xlabel("Emoções")
+    ax1.set_ylabel("Número de ativações")
+    ax1.tick_params(axis="x", rotation=45)
 
     # Adiciona o texto da emoção predominante no subplot do gráfico de barras
-    ax1.text(0.5, 1.1, f'Emoção predominante: {dominant_emotion_video}', 
-            horizontalalignment='center', verticalalignment='center', 
-            transform=ax1.transAxes, fontsize=12, color='blue')
+    ax1.text(
+        0.5,
+        1.1,
+        f"Emoção predominante: {dominant_emotion_video}",
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax1.transAxes,
+        fontsize=12,
+        color="blue",
+    )
 
     # Carrega a imagem do frame
-    frame_image = Image.open(dominant_frame_path)  # Substitua pelo caminho correto da imagem
+    frame_image = Image.open(
+        dominant_frame_path
+    )  # Substitua pelo caminho correto da imagem
     frame_image = frame_image.resize((300, 300))  # Redimensiona a imagem, se necessário
 
     # Adiciona a imagem no segundo subplot
@@ -98,18 +116,34 @@ def analyze_video_emotions(video_path, threshold=0.6):
     ax2.add_artist(ab)
 
     # Adiciona o texto do frame com maior expressão emocional em cima da imagem
-    ax2.text(0.5, 1.05, f'Frame com maior expressão emocional: {dominant_frame}', 
-            horizontalalignment='center', verticalalignment='center', 
-            transform=ax2.transAxes, fontsize=12, color='green')
+    ax2.text(
+        0.5,
+        1.05,
+        f"Frame com maior expressão emocional: {dominant_frame}",
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax2.transAxes,
+        fontsize=12,
+        color="green",
+    )
 
     # Remove os eixos do subplot da imagem
-    ax2.axis('off')
+    ax2.axis("off")
 
     # Ajusta o layout para evitar cortes
     plt.tight_layout()
 
     # Salva a figura
-    plt.savefig(f"graphs/{video_name}_emotions.png", bbox_inches='tight')
+    # verifique se a pasta 'graphs' existe, se não, crie
+    if not os.path.exists("graphs"):
+        os.makedirs("graphs")
+    # verifica se a pasta do ator existe, se não, cria
+    if not os.path.exists(f"graphs/{video_name.replace('.csv','').split('-')[-1]}"):
+        os.makedirs(f"graphs/{video_name.replace('.csv','').split('-')[-1]}")
+
+    plt.savefig(
+        f"graphs/{video_name.replace('.csv','').split('-')[-1]}/{video_name.replace('.csv', '')}.png"
+    )
     plt.close()  # Fecha a figura
     # Exibe o gráfico
     # plt.show()
@@ -117,8 +151,8 @@ def analyze_video_emotions(video_path, threshold=0.6):
 
 if __name__ == "__main__":
     # Caminho da pasta onde estão os vídeos
-    video_directory = "/Users/yurigarciaribeiro/Documents/TCC/DockerC/output"
-    
+    video_directory = "C:/Users/yurig/Documents/GitHub/TCC/dataset"
+
     # Obter todos os arquivos CSV recursivamente
     video_files = list_all_files_in_directory(video_directory)
 
