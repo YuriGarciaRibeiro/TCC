9504d8fc0b6ec7380e7ba7643e57d2389d194683
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2025-03-09 23:06:40 -0300

    Adicionar script para processamento de arquivos de áudio .wav e geração de animações .usd

---
+++ src/animation_generator.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+import py_audio2face as pya2f
+
+
+def process_audio_files(directory):
+    a2f = pya2f.Audio2Face()
+    # Percorre todos os arquivos e subpastas no diretório especificado
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # Verifica se o arquivo é um arquivo .wav
+            if file.endswith(".wav"):
+                # Caminho completo do arquivo de áudio
+                audio_file_path = os.path.join(root, file)
+
+                # Caminho de saída com a mesma estrutura de diretórios e nome, mas com extensão .usd
+                output_path = (
+                    os.path.splitext(audio_file_path)[0] + "_animation" + ".usd"
+                )
+
+                # Executa a função a2f.audio2face_single
+                final_path = a2f.audio2face_single(
+                    audio_file_path=audio_file_path,
+                    output_path=output_path,
+                    fps=60,
+                    emotion_auto_detect=True,
+                )
+                print(f"Processed: {audio_file_path} -> {final_path}")
+
+# Substitua 'your_directory_path' pelo caminho da pasta que você deseja processar
+directory_path = r"C:\Users\yurig\Documents\GitHub\TCC\dataset"
+process_audio_files(directory_path)
