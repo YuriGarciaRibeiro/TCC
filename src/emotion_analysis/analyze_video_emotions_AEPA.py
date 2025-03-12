c138244ecbc6604b2cce482ef515cb93a5ad9569
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2025-03-12 20:21:18 -0300

    refatoraçao de codigo e renomeação de arquivos

---
+++ src/emotion_analysis/analyze_video_emotions_AEPA.py
@@ -0,0 +1,73 @@
+
+# ? (Análise de Emoções por Predominância de AUs)
+# ? AEPA
+
+import os
+import re
+import pandas as pd
+from emotion_aus import emotions_au
+from generate_plot import generate_plot  # Importa a função de geração do plot
+
+def list_all_files_in_directory(directory_path):
+    all_files = []
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith(".csv"):
+                all_files.append(os.path.join(root, file))
+    return all_files
+
+def break_string(s):
+    pattern = r"(?P<type>full-AV-speech)-(?P<emotion>\w+)-(?P<intensity>\w+)-(?P<phrase>[\w\s-]+)-(?P<repetition>\d+[a-zA-Z-]+)-(?P<actor>actor\d+)"
+    result = re.match(pattern, s)
+    if result:
+        return result.groupdict()
+    else:
+        return None
+
+def analyze_video_emotions(video_path, threshold=0.6):
+    df = pd.read_csv(video_path)
+    filtered_df = df[[col for col in df.columns if col.startswith(" AU") and col.endswith("r")]]
+    frame_emotions = {}
+    emotion_counts = {emotion: 0 for emotion in emotions_au}
+    dominant_frame = 0
+
+    for idx, row in filtered_df.iterrows():
+        emotion_sums = {emotion: 0 for emotion in emotions_au}
+        for emotion, aus in emotions_au.items():
+            for au in aus:
+                col_name = f" AU{au:02}_r"
+                if col_name in row and row[col_name] > threshold:
+                    emotion_sums[emotion] += row[col_name]
+        dominant_emotion = max(emotion_sums, key=emotion_sums.get)
+        frame_emotions[idx] = dominant_emotion
+        emotion_counts[dominant_emotion] += 1
+        if emotion_sums[dominant_emotion] > emotion_sums.get(dominant_frame, 0):
+            dominant_frame = idx
+
+    dominant_frame_path = f"{video_path.replace('.csv', '')}_aligned/frame_det_00_{str(dominant_frame).zfill(6)}.bmp"
+    dominant_emotion_video = max(emotion_counts, key=emotion_counts.get)
+    video_name = os.path.basename(video_path)
+    data = break_string(video_name)
+
+    # Retorna os dados para o plot
+    return {
+        "emotion_counts": emotion_counts,
+        "dominant_emotion_video": dominant_emotion_video,
+        "dominant_frame": dominant_frame,
+        "dominant_frame_path": dominant_frame_path,
+        "video_name": video_name,
+        "data": data
+    }
+
+if __name__ == "__main__":
+    # Caminho da pasta onde estão os vídeos
+    video_directory = "/Users/yurigarciaribeiro/Documents/GitHub/TCC/dataset"
+
+    # Obter todos os arquivos CSV recursivamente
+    video_files = list_all_files_in_directory(video_directory)
+
+    # Rodar a função para cada arquivo de vídeo (CSV)
+    for video in video_files:
+        if video.endswith(".csv"):  # Verifique se o arquivo é CSV
+            analysis_result = analyze_video_emotions(video)
+            generate_plot(analysis_result,"AEPA")  # Gera o plot com os dados analisados
---
+++ src/emotion_analysis/generate_plot.py
@@ -0,0 +1,53 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+from PIL import Image
+
+def generate_plot(analysis_result,sufix = ""):
+    emotion_counts = analysis_result["emotion_counts"]
+    dominant_emotion_video = analysis_result["dominant_emotion_video"]
+    dominant_frame = analysis_result["dominant_frame"]
+    dominant_frame_path = analysis_result["dominant_frame_path"]
+    video_name = analysis_result["video_name"]
+    data = analysis_result["data"]
+
+    fig = plt.figure(figsize=(16, 10))
+    gs = gridspec.GridSpec(2, 2, width_ratios=[1, 1], height_ratios=[2, 1])
+
+    ax1 = plt.subplot(gs[0, 0])
+    ax2 = plt.subplot(gs[0, 1])
+    ax3 = plt.subplot(gs[1, 0])
+
+    plt.suptitle(f"Contagem das Emoções ao Longo do Vídeo: {video_name}", fontsize=16)
+
+    ax1.bar(emotion_counts.keys(), emotion_counts.values())
+    ax1.set_xlabel("Emoções")
+    ax1.set_ylabel("Número de ativações")
+    ax1.tick_params(axis="x", rotation=45)
+    ax1.text(0.5, 1.1, f"Emoção predominante: {dominant_emotion_video}", horizontalalignment="center", verticalalignment="center", transform=ax1.transAxes, fontsize=12, color="blue")
+
+    frame_image = Image.open(dominant_frame_path)
+    frame_image = frame_image.resize((300, 300))
+    imagebox = OffsetImage(frame_image, zoom=0.8)
+    ab = AnnotationBbox(imagebox, (0.5, 0.5), frameon=False)
+    ax2.add_artist(ab)
+    ax2.text(0.5, 1.05, f"Frame com maior expressão emocional: {dominant_frame}", horizontalalignment="center", verticalalignment="center", transform=ax2.transAxes, fontsize=12, color="green")
+    ax2.axis("off")
+
+    cell_text = [[key, value] for key, value in data.items()]
+    table = ax3.table(cellText=cell_text, colLabels=["Chave", "Valor"], loc="center", cellLoc="center")
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.scale(1, 1.5)
+    ax3.axis("off")
+
+    # aumenta o tamanho do plot
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.9)
+
+    if not os.path.exists(f"/Users/yurigarciaribeiro/Documents/GitHub/TCC/graphs/{video_name.replace('.csv','').split('-')[-1]}"):
+        os.makedirs(f"/Users/yurigarciaribeiro/Documents/GitHub/TCC/graphs/{video_name.replace('.csv','').split('-')[-1]}")
+
+    plt.savefig(f"/Users/yurigarciaribeiro/Documents/GitHub/TCC/graphs/{video_name.replace('.csv','').split('-')[-1]}/{video_name.replace('.csv', '')}_{sufix}.png")
+    plt.close()
